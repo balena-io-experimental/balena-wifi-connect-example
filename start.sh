d837310571fa58f52060c4db864e209d9f92361c
Joseph Roberts <j.baldwin.roberts@gmail.com>
2016-12-13 13:39:32 +0000

    Integrated resin-wifi-connect

--- start.sh
+++ start.sh
@@ -3,5 +3,12 @@
 # Enable i2c
 modprobe i2c-dev
 
-# Start the python application
-python src/main.py
+# Start the pre application
+python src/pre.py
+
+# Start resin-wifi-connect
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+sleep 1
+node resin-wifi-connect/src/app.js --clear=false
+
+# Start the post application
