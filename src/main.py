292cc11be22a847f60af248d980512c810f2854f
majorz <zahari@resin.io>
2017-10-23 12:21:21 +0300

    Support resin-wifi-connect v3
    Connects-To: #3

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ def handle_button(ch, evt):
     # flag set to 'true'. This forces resin-wifi-connect to remove any
     # previously configured WiFi connections.
     print("Button pressed")
-    subprocess.call(["node", "resin-wifi-connect/src/app.js", "--clear=true"])
+    subprocess.call(["resin-wifi-connect", "--clear=true"])
 
 
 if __name__ == "__main__":
